f9ab1c0f38a0d950855647dc04b767eefc84eb10
patriciax <yively.pa@gmail.com>
2021-09-08 19:26:45 -0400

    change img

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,8 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--begin::Layout Themes(used by all pages)-->
 		<!--end::Layout Themes-->
 		<!--<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />-->
-		<link rel="shortcut icon" type="image/x-icon" href="https://celesteypiedra.com/assets/img/iso.png">
+	<!--	<link rel="shortcut icon" type="image/x-icon" href="https://www.laliberty.com.co/coming/assets/img/favicon.png">-->
+		<link rel="shortcut icon" type="image/x-icon"  href="assets/media/logo-black.png" />
 
 		<script>
 
